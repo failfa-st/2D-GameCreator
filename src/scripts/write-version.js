99c964b83533dd44379639fa46dd1a5963dc82e7
Tim Pietrusky <timpietrusky@gmail.com>
2023-06-01 15:49:08 +0200

    fix(write-version): move the script into src so that it's available when building the docker image

--- src/scripts/write-version.js
+++ src/scripts/write-version.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const packageJson = require('../package.json');
+const packageJson = require('../../package.json');
 
 const envVars = `NEXT_PUBLIC_VERSION=${packageJson.version}\n`;
 

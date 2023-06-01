fa302b928f62957d0667a06f5e3b26fc4369be40
Tim Pietrusky <timpietrusky@gmail.com>
2023-06-01 11:08:05 +0200

    fix: error handling

--- src/pages/api/generate.ts
+++ src/pages/api/generate.ts
@@ -3,6 +3,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { AxiosError } from "axios";
 import process from "node:process";
 import { createClient } from "@/services/api/openai";
+import { CustomAxiosError } from "@/services/api/axios";
 
 export default async function handler(request: NextApiRequest, response: NextApiResponse) {
 	switch (request.method) {
@@ -13,7 +14,9 @@ export default async function handler(request: NextApiRequest, response: NextApi
 				const answer = await toOpenAI({ ...request.body, client });
 				return response.status(200).json(answer);
 			} catch (error) {
-				return response.status((error as AxiosError).status ?? 500).json(error);
+				return response.status((error as AxiosError).status ?? 500).json({
+					message: (error as CustomAxiosError).data?.error?.message ?? "UNKNOWN",
+				});
 			}
 		default:
 			return response.status(405).json({});
